e6458f550c4243a6b7b5f68c00a7ec7c7c10ce51
zenjl <minhle7206@gmail.com>
2023-09-06 20:53:34 +0700

    1st commit

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ const server = http.createServer((req, res) => {
 
   // step 2
   const url = req.url;
+  const method = req.method;
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunkData) => {
+      console.log(chunkData);
+      body.push(chunkData);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
+    return;
+  }
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>My web</title></head>");
