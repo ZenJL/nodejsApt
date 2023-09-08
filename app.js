47405e0f2f4e50a425f3298abc4d701c110843b7
zenjl <minhle7206@gmail.com>
2023-09-08 20:33:56 +0700

    express, app.ise ordering and app.listen

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ const express = require("express");
 
 const app = express();
 
-// // method createServer
-// const server = http.createServer((req, res) => {
-//   route(req, res);
-// });
-
 // Handle logic req, res
-app.use((req, res, next) => {});
+// Middleware
+app.use((req, res, next) => {
+  console.log("always run");
+  next();
+});
 
-const server = http.createServer(app);
+app.use("/add-product", (req, res, next) => {
+  console.log("2222");
+  res.send("<h1>Hello from Express</h1>");
+});
 
-server.listen(3001);
+app.use("/", (req, res, next) => {
+  console.log("1111");
+});
+
+// const server = http.createServer(app);
+// Express can run directly without createServer
+
+// server.listen(3001);
+app.listen(3001);
